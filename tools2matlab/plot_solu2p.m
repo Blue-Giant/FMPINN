87f6fb1e9a9483eba70bdfa2ebfcef836771b096
Blue-Giant <42233512+Blue-Giant@users.noreply.github.com>
2023-07-08 13:02:43 +0800

    Add files via upload

---
+++ tools2matlab/plot_solu2p.m
@@ -0,0 +1,33 @@
+clc;
+clear all
+close all
+
+q = 7;
+nl = 2;
+T = [];
+J = [];
+
+% geom: (only square geometry available now)
+% generating 2d square mesh for the region [-1, 1] x [-1 1]
+geom.q = q;
+geom.nl = nl;
+geom.L = 2; % side length 
+geom.dim = 2; % dimension of the problem
+geom.m = 2^geom.dim; % 
+geom.N1 = 2^q; % dofs in one dimension
+geom.N = (geom.m)^geom.q; % dofs in the domain
+geom.h = geom.L/(geom.N1+1); % grid size
+geom.xstart = 0;
+geom.xend = 1;
+geom.ystart = 0;
+geom.yend = 1;
+
+geom = assemble_fmesh(geom);
+
+meshX = geom.pin(1,:);
+meshY = geom.pin(2,:);
+Ptrue = (2*meshX-1).*(2*meshY-1);
+
+figure('name','Exact_P')
+mesh_solu2Ptrue = plot_fun2in(geom,Ptrue);
+hold on
